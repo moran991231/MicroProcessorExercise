4b937380b0db1b28db2fda778ebf8982c13ed622
moran991231 <moran991231@naver.com>
2021-09-26 16:19:00 +0900

    week4: segment (for report)

--- week4/app/sm9s5422_segment_test.c
+++ week4/app/sm9s5422_segment_test.c
@@ -1,156 +1,140 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
 #include <termios.h>
+#include <time.h>
+#include <unistd.h>
 
 static struct termios initial_settings, new_settings;
 static int peek_character = -1;
 
 void init_keyboard() {
-	tcgetattr(0,&initial_settings);
-	new_settings = initial_settings;
-	new_settings.c_lflag &= ~ICANON;
-	new_settings.c_lflag &= ~ECHO;
-	new_settings.c_lflag &= ~ISIG;
-	new_settings.c_cc[VMIN] = 1;
-	new_settings.c_cc[VTIME] = 0;
-	tcsetattr(0,TCSANOW,&new_settings);
+    tcgetattr(0, &initial_settings);
+    new_settings = initial_settings;
+    new_settings.c_lflag &= ~ICANON;
+    new_settings.c_lflag &= ~ECHO;
+    new_settings.c_lflag &= ~ISIG;
+    new_settings.c_cc[VMIN] = 1;
+    new_settings.c_cc[VTIME] = 0;
+    tcsetattr(0, TCSANOW, &new_settings);
 }
 
-void close_keyboard() {
-	tcsetattr(0,TCSANOW,&initial_settings);
-}
+void close_keyboard() { tcsetattr(0, TCSANOW, &initial_settings); }
 
 int kbhit() {
-	char ch;
-	int nread;
+    char ch;
+    int nread;
 
-	if(peek_character != -1) return 1;
+    if (peek_character != -1) return 1;
 
-	new_settings.c_cc[VMIN] = 0;
-	tcsetattr(0,TCSANOW,&new_settings);
-	nread = read(0,&ch,1);
-	new_settings.c_cc[VMIN] = 1;
-	tcsetattr(0,TCSANOW,&new_settings);
+    new_settings.c_cc[VMIN] = 0;
+    tcsetattr(0, TCSANOW, &new_settings);
+    nread = read(0, &ch, 1);
+    new_settings.c_cc[VMIN] = 1;
+    tcsetattr(0, TCSANOW, &new_settings);
 
-	if(nread == 1) {
-		peek_character = ch;
-		return 1;
-	}
-	return 0;
+    if (nread == 1) {
+        peek_character = ch;
+        return 1;
+    }
+    return 0;
 }
 
 int readch() {
-	char ch;
+    char ch;
 
-	if(peek_character != -1) {
-		ch = peek_character;
-		peek_character = -1;
-		return ch;
-	}
-	read(0,&ch,1);
-	return ch;
+    if (peek_character != -1) {
+        ch = peek_character;
+        peek_character = -1;
+        return ch;
+    }
+    read(0, &ch, 1);
+    return ch;
 }
 
-int main(int argc, char * argv[]) {
-	int fd, value=1, i, count, temp ,ch;
-	unsigned short input, dir =0;
-	char temp_val[6] ={0};
+void print_info() {
+    printf(" --------------------\n");
+    printf("   7 segment Program \n");
+    printf(" --------------------\n");
+    printf("    [c] counter      \n");
+    printf("    [q] exit         \n");
+    printf(" --------------------\n\n");
+}
+void print_info_command() {
+    printf(" --------------------\n");
+    printf("        Counter      \n");
+    printf(" --------------------\n");
+    printf("    [p] pause        \n");
+    printf("    [c] continue     \n");
+    printf("    [r] reset        \n");
+    printf("    [q] exit         \n");
+    printf(" --------------------\n\n");
+}
 
-	if((fd=open("/dev/sm9s5422_segment", O_RDWR|O_SYNC))<0){
-		printf("FND open failed\n");
-		exit(1);
-	}
+#define NUM_WRITE 14
 
-	init_keyboard();
+int counter_program(int fd) {
+    int count, value, i;
+    unsigned short dir;
+    char temp_val[6] = {0};
+    printf("Input counter value (0: exit program, 999999~01 ) :");
+    close_keyboard();
+    scanf("%d", &value);  // get number to count
+    init_keyboard();
+    if (value == 0) return -1;  // if 0, exit
 
-	printf(" --------------------\n");
-	printf("   7 segment Program \n");
-	printf(" --------------------\n");
-	printf("    [c] counter      \n");
-	printf("    [q] exit         \n");
-	printf(" --------------------\n\n");
+    print_info_command();
+    ioctl(fd, 0, NULL, NULL);
+    dir = 'r';
 
-	while( input !='q'){
-		input ='r';
-		if(kbhit()){
-			ch = readch();
-			switch (ch)
-			{
-			case 'c':
-			case 'q':
-				input=ch;
-				break;			 
-			}
-		}
+    while (dir != 'q') {
+        // get command continue(c), puase(p), reset(r), exit(q)
+        if (kbhit()) dir = readch();
 
-	switch(input){
-		case 'c':
-			printf("Input conter value (0: exit program, 999999~01 ) :");
-			close_keyboard();
-			scanf("%d", &value);
-			init_keyboard();
-			count = value;
-			
-			printf(" --------------------\n");
-			printf("        Counter      \n");
-			printf(" --------------------\n");
-			printf("    [p] pause      \n");
-			printf("    [c] continue    \n");
-			printf("    [r] reset    \n");
-			printf("    [q] exit    \n");
-			printf(" --------------------\n\n");
-			ioctl(fd, 0, NULL, NULL);
-			dir='c';
+        if (dir == 'c') {
+            if (count <= 0) return 0;  // count down ended
+            count--;
+        } else if (dir == 'r') {
+            count = value;
+            dir = 'c';
+        }  // else if (dir = 'p') {} do nothing especially
 
-			while(dir != 'q'){
-				int temp = count;
-				for(int i=5; 0<=i; i--){
-					temp_val[i] = temp%10;
-					temp /=10;
-				}
+        // write decimal digit on temp_val[]
+        int temp = count;
+        for (int i = 5; 0 <= i; i--) {
+            temp_val[i] = temp % 10;
+            temp /= 10;
+        }
+        // write segment
+        for (i = 0; i < NUM_WRITE; i++) write(fd, &temp_val, 6);
+    }
+    return 0;
+}
+int main(int argc, char *argv[]) {
+    int fd, value = 1, i, temp, ch;
+    unsigned short input;
 
-				if(dir=='c'){
-					if(count <= 0) break;
-					for( i=0; i<14; i++)
-						write(fd, &temp_val, 6);
-					count--;
-				}else if(dir=='r'){
-					count=value;
-					dir='c';
-				}else if (dir='p'){
-					for(i=0; i<14; i++)
-						write(fd, &temp_val, 6);
-				}
-				if(kbhit()){
-					ch = readch();
-					switch(ch){
-						case 'c':
-						case 'p':
-						case 'r':
-						case 'q':
-							dir=ch;
-							break;
-					}
-				}
-			}
-			
-		printf(" --------------------\n");
-		printf("   7 segment Program \n");
-		printf(" --------------------\n");
-		printf("    [c] counter      \n");
-		printf("    [q] exit         \n");
-		printf(" --------------------\n\n");
-		break;
+    if ((fd = open("/dev/sm9s5422_segment", O_RDWR | O_SYNC)) < 0) {
+        printf("FND open failed\n");
+        exit(1);
+    }
+    init_keyboard();
+    print_info();
 
-	case 'q':
-		break;
-	}
-	}
-	
-	close_keyboard();
-	close(fd);
-		return 0;
+    while (input != 'q') {
+        if (kbhit()) input = readch();
+
+        switch (input) {
+            case 'c':
+                if (counter_program(fd) == -1) {
+                    input = 'q';
+                    break;
+                }
+                print_info();
+                break;
+        }
+    }
+    close_keyboard();
+    close(fd);
+    return 0;
 }
--- week4/device_driver/sm9s5422_segment.c
+++ week4/device_driver/sm9s5422_segment.c
@@ -1,218 +1,200 @@
-#include <linux/miscdevice.h>
 #include <asm/uaccess.h>
-#include <linux/init.h>
-#include <linux/fs.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/errno.h>
-#include <linux/types.h>
-#include <linux/ioport.h>
+#include <hanback/gpios.h>
 #include <linux/delay.h>
+#include <linux/errno.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/ioport.h>
+#include <linux/kernel.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/of_gpio.h>
+#include <linux/types.h>
 #include <mach/gpio.h>
 #include <plat/gpio-cfg.h>
-#include <linux/of_gpio.h>
-#include <hanback/gpios.h>
 
 unsigned int ibuf[8];
 
-int Getsegmentcode_base (int x)
-{
-  unsigned int i;
+int Getsegmentcode_base(int x) {
+    unsigned int i;
 
-  for (i = 0; i < 8; i++)
-    ibuf[i] = 0;
+    for (i = 0; i < 8; i++) ibuf[i] = 0;
 
-  switch (x) {
-    case 0 :
-      for (i=0; i<6; i++) ibuf[i] = 1;
-      break;
+    switch (x) {
+        case 0:
+            for (i = 0; i < 6; i++) ibuf[i] = 1;
+            break;
 
-    case 1 : ibuf[1] = 1; ibuf[2] = 1; break;
+        case 1:
+            ibuf[1] = 1;
+            ibuf[2] = 1;
+            break;
 
-    case 2 :
-      for (i=0; i<2; i++) ibuf[i] = 1;
-      for (i=3; i<5; i++) ibuf[i] = 1;
-      ibuf[6] = 1;
-      break;
+        case 2:
+            for (i = 0; i < 2; i++) ibuf[i] = 1;
+            for (i = 3; i < 5; i++) ibuf[i] = 1;
+            ibuf[6] = 1;
+            break;
 
-    case 3 :
-      for (i=0; i<4; i++) ibuf[i] = 1;
-      ibuf[6] = 1;
-      break;
+        case 3:
+            for (i = 0; i < 4; i++) ibuf[i] = 1;
+            ibuf[6] = 1;
+            break;
 
-    case 4 :
-      for (i=1; i<3; i++) ibuf[i] = 1;
-      for (i=5; i<7; i++) ibuf[i] = 1;
-      break;
+        case 4:
+            for (i = 1; i < 3; i++) ibuf[i] = 1;
+            for (i = 5; i < 7; i++) ibuf[i] = 1;
+            break;
 
-    case 5 :
-      ibuf[0] = 1;
-      for (i=2; i<4; i++) ibuf[i] = 1;
-      for (i=5; i<7; i++) ibuf[i] = 1;
-      break;
+        case 5:
+            ibuf[0] = 1;
+            for (i = 2; i < 4; i++) ibuf[i] = 1;
+            for (i = 5; i < 7; i++) ibuf[i] = 1;
+            break;
 
-    case 6 :
-      for (i=2; i<7; i++) ibuf[i] = 1;
-      break;
+        case 6:
+            for (i = 2; i < 7; i++) ibuf[i] = 1;
+            break;
 
-    case 7 :
-      for (i=0; i<3; i++) ibuf[i] = 1;
-      ibuf[5] = 1;
-      break;
+        case 7:
+            for (i = 0; i < 3; i++) ibuf[i] = 1;
+            ibuf[5] = 1;
+            break;
 
-    case 8 :
-      for (i=0; i<7; i++) ibuf[i] = 1;
-      break;
+        case 8:
+            for (i = 0; i < 7; i++) ibuf[i] = 1;
+            break;
 
-    case 9 :
-      for (i=0; i<4; i++) ibuf[i] = 1;
-      for (i=5; i<7; i++) ibuf[i] = 1;
-      break;
+        case 9:
+            for (i = 0; i < 4; i++) ibuf[i] = 1;
+            for (i = 5; i < 7; i++) ibuf[i] = 1;
+            break;
 
-    case 10 :
-      for (i=0; i<3; i++) ibuf[i] = 1;
-      for (i=4; i<8; i++) ibuf[i] = 1;
-      break;
-    case 11 :
-      for (i=0; i<8; i++) ibuf[i] = 1;
-      break;
-    case 12 :
-      for (i=3; i<6; i++) ibuf[i] = 1;
-      ibuf[0] = 1;
-      ibuf[7] = 1;
-      break;
-    case 13 :
-      ibuf[7] = 1;
-      for (i=0; i<6; i++) ibuf[i] = 1;
-      break;
-    case 14 :
-      for (i=3; i<8; i++) ibuf[i] = 1;
-      ibuf[0] = 1;
-      break;
-    case 15 :
-      for (i=4; i<8; i++) ibuf[i] = 1;
-      ibuf[0] = 1;
-      break;
+        case 10:
+            for (i = 0; i < 3; i++) ibuf[i] = 1;
+            for (i = 4; i < 8; i++) ibuf[i] = 1;
+            break;
+        case 11:
+            for (i = 0; i < 8; i++) ibuf[i] = 1;
+            break;
+        case 12:
+            for (i = 3; i < 6; i++) ibuf[i] = 1;
+            ibuf[0] = 1;
+            ibuf[7] = 1;
+            break;
+        case 13:
+            ibuf[7] = 1;
+            for (i = 0; i < 6; i++) ibuf[i] = 1;
+            break;
+        case 14:
+            for (i = 3; i < 8; i++) ibuf[i] = 1;
+            ibuf[0] = 1;
+            break;
+        case 15:
+            for (i = 4; i < 8; i++) ibuf[i] = 1;
+            ibuf[0] = 1;
+            break;
 
-    default :
-      for (i=0; i<8; i++) ibuf[i] = 1;
-      break;
-  }
-  return 0;
+        default:
+            for (i = 0; i < 8; i++) ibuf[i] = 1;
+            break;
+    }
+    return 0;
 }
-	
-static int sm9s5422_segment_open(struct inode * inode, struct file * file){
-	printk("sm9s5422_segment_open, \n");
 
-  int err, i;
+static int sm9s5422_segment_open(struct inode* inode, struct file* file) {
+    int err, i;
+    printk("sm9s5422_segment_open, \n");
+    for (i = 0; i < 8; i++) {
+        err = gpio_request(gpe0(i), "GPE0");
+        if (err) printk("segment.c failed to request GPE0(%d) \n", i);
+        s3c_gpio_setpull(gpe0(i), S3C_GPIO_PULL_NONE);
+        gpio_direction_output(gpe0(i), 0);
+    }
+    for (i = 0; i < 7; i++) {
+        if (i == 5) continue;
+        err = gpio_request(gpg1(i), "GPG1");
+        if (err) printk("segment.c failed to request GPG1(%d) \n", i);
+        s3c_gpio_setpull(gpg1(i), S3C_GPIO_PULL_UP);
+        gpio_direction_output(gpg1(i), 1);
+    }
+    return 0;
+}
 
-  for(i=0; i<8; i++){
-    err = gpio_request(gpe0(i), "GPE0");
-    if(err) 
-      printk("segment.c failed to request GPE0(%d) \n", i);
-    s3c_gpio_setpull(gpe0(i), S3C_GPIO_PULL_NONE);
-    gpio_direction_output(gpe0(i),0);
+static int sm9s5422_segment_release(struct inode* inode, struct file* file) {
+    int i;
+    printk("sm9s5422_segment_release, \n");
 
-  }
-    for(i=0; i<7; i++){
-      if(i==5) continue;
-      err = gpio_request(gpg1(i),"GPG1");
-      if(err)
-        printk("segment.c failed to request GPG1(%d) \n",i);
-      s3c_gpio_setpull(gpg1(i), S3C_GPIO_PULL_UP);
-      gpio_direction_output(gpg1(i), 1);
+    for (i = 0; i < 8; i++) gpio_free(gpe0(i));
+    for (i = 0; i < 7; i++)
+        if (i != 5) gpio_free(gpg1(i));
+    return 0;
+}
 
+#define MDELAY_ 20
+static ssize_t sm9s5422_segment_write(struct file* file, const char* buf, size_t length, loff_t* ofs) {
+    printk("sm9s5422_segment_write, \n");
+
+    unsigned int ret, i, j;
+    unsigned char data[6];
+    ret = copy_from_user(data, buf, 6);
+    // there're 6 digits (gpg1(0,1,2,3,4,6) don't use 5)
+    for (j = 0; j < 5; j++) {  // 1~5-th digits
+
+        // write segment on/off data on ibuf[]
+        Getsegmentcode_base((int)data[j]);
+
+        // off all segments in a digit
+        for (i = 0; i < 8; i++) gpio_direction_output(gpe0(i), 0);
+
+        // select digit
+        if (j > 0) gpio_direction_output(gpg1(j - 1), 1);
+        gpio_direction_output(gpg1(j), 0);
+
+        // set segment on/off according to ibuf[]
+        for (i = 0; i < 8; i++) gpio_direction_output(gpe0(i), ibuf[i]);
+
+        mdelay(MDELAY_);  // delay
     }
 
-  return 0;
-}
+    // 6-th digit
+    Getsegmentcode_base((int)data[5]);
 
+    for (i = 0; i < 8; i++) gpio_direction_output(gpe0(i), 0);
 
+    gpio_direction_output(gpg1(4), 1);
+    gpio_direction_output(gpg1(6), 0);
+    for (i = 0; i < 8; i++) gpio_direction_output(gpe0(i), ibuf[i]);
 
+    mdelay(MDELAY_);
+    gpio_direction_output(gpg1(6), 1);
 
-static int sm9s5422_segment_release(struct inode * inode, struct file * file){
-  printk("sm9s5422_segment_release, \n");
-  
-  int i;
-
-  for(i=0; i<8; i++)
-    gpio_free(gpe0(i));
-
-  for(i=0; i<7; i++)
-    if(i!=5)
-      gpio_free(gpg1(i));
-  
-  return 0;
-}
-
-
-
-static ssize_t sm9s5422_segment_write(struct file * file, const char * buf,
-		size_t length, loff_t * ofs) {
-	printk("sm9s5422_segment_write, \n");
-
-	unsigned int ret, i, j;
-	unsigned char data[6];
-	ret = copy_from_user(data, buf, 6);
-
-  for(j=0; j<5; j++){
-    Getsegmentcode_base((unsigned int) data[j]);
-    for(i=0; i<8; i++)
-      gpio_direction_output(gpe0(i),0);
-
-    if(j>0)
-      gpio_direction_output(gpg1(j-1),1);
-
-    gpio_direction_output(gpg1(j),0);
-
-    for(i=0; i<8; i++)
-      gpio_direction_output(gpe0(i), (unsigned int) ibuf[i]);
-
-    mdelay(2);
-  }
-
-  Getsegmentcode_base((unsigned int) data[5]);
-
-  for(i=0; i<8; i++)
-    gpio_direction_output(gpe0(i),0);
-
-  gpio_direction_output(gpg1(4), 1);
-  gpio_direction_output(gpg1(6),0);
-  for(i=0; i<8; i++)
-  gpio_direction_output(gpe0(i), (unsigned int) ibuf[i]);
-
-  mdelay(2);
-
-  gpio_direction_output(gpg1(6), 1);
-
-  return length;
-
+    return length;
 }
 
 static struct file_operations sm9s5422_segment_fops = {
-	.owner = THIS_MODULE,
-	.open = sm9s5422_segment_open,
-	.release = sm9s5422_segment_release,
-	.write = sm9s5422_segment_write,
+    .owner = THIS_MODULE,
+    .open = sm9s5422_segment_open,
+    .release = sm9s5422_segment_release,
+    .write = sm9s5422_segment_write,
 };
 
 static struct miscdevice sm9s5422_segment_driver = {
-	.minor = MISC_DYNAMIC_MINOR,
-	.name = "sm9s5422_segment",
-	.fops = &sm9s5422_segment_fops,
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "sm9s5422_segment",
+    .fops = &sm9s5422_segment_fops,
 };
 
-static int sm9s5422_segment_init(void){
-	printk("sm9s5422_segment_init, \n");
-	
-	return misc_register(&sm9s5422_segment_driver);
+static int sm9s5422_segment_init(void) {
+    printk("sm9s5422_segment_init, \n");
+
+    return misc_register(&sm9s5422_segment_driver);
 }
 
-static void sm9s5422_segment_exit(void){
-	printk("sm9s5422_segment_exit, \n");
+static void sm9s5422_segment_exit(void) {
+    printk("sm9s5422_segment_exit, \n");
 
-	misc_deregister(&sm9s5422_segment_driver);
-	
+    misc_deregister(&sm9s5422_segment_driver);
 }
 
 module_init(sm9s5422_segment_init);

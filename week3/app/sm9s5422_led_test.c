13a0743ed6244d144959fff3616aee2fa3961a44
moran991231 <moran991231@naver.com>
2021-09-26 16:18:47 +0900

    week3: led (for report)

--- week3/app/sm9s5422_led_test.c
+++ week3/app/sm9s5422_led_test.c
@@ -1,43 +1,37 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <sys/types.h> 
-#include<sys/stat.h>
-#include <fcntl.h>
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[]) {
     int dev, i;
-    char temp, buf[] = {0};
-    unsigned char t =0;
+    char temp, buf[8] = {0};  // here
+    unsigned char t = 0;
 
-    if(argc<=1){
+    if (argc <= 1) {  // get led states from parameters
         printf("please input the parameter! ex) ./test 0xff \n");
         return -1;
     }
-    dev = open("/dev/sm9s5422_led",O_WRONLY);
+    dev = open("/dev/sm9s5422_led", O_WRONLY);
 
-    if(dev != -1){
-        if(argv[1][0] == '0' &&(argv[1][1] == 'x'|| argv[1][1] == 'X') )
-        {
-            temp = strtol(argv[1], NULL, 16);
-        }else{
+    if (dev != -1) {
+        if (argv[1][0] == '0' && (argv[1][1] == 'x' || argv[1][1] == 'X')) {
+            // if input is hexa-decimal
+            temp = (char)strtol(argv[1], NULL, 16);
+        } else {
+            // if input is decimal
             temp = atoi(argv[1]);
         }
-        printf("temp: %d  \n",(int)temp);
-        for(int i=0; i<8; i++){
-            buf[i] = (char)((temp>>i)%2);
-            printf("%d",(int)buf[i]);
-        }
-        printf("\n");
-        
-    }else{
+
+        for (int i = 0; i < 8; i++)  // i-th bit in temp=> buf[i]
+            buf[i] = (char)((temp >> i) & 1);
+    } else {
         printf("Device Open Error \n");
         return -1;
     }
-
     write(dev, buf, 8);
     close(dev);
-    
     return 0;
-    
 }
--- week3/device_driver/sm9s5422_led.c
+++ week3/device_driver/sm9s5422_led.c
@@ -14,78 +14,67 @@
 #include <linux/of_gpio.h>
 #include <hanback/gpios.h>
 #include <linux/delay.h>
-	
-static int sm9s5422_led_open(struct inode * inode, struct file * file){
-	int err=0,i=0;
 
-	printk(KERN_CRIT"sm9s5422_led_open, \n");
-	printk("2018440059 Park Jaesun \n");
-
-
-	for (i=0; i<8; i++)
-	{
-		err = gpio_request(gpy7(i), "Led");
-		if(err)
-			printk("led.c failed to request gpy7(%d) \n", i);
-		s3c_gpio_setpull(gpy7(i), S3C_GPIO_PULL_NONE);
-		gpio_direction_output(gpy7(i), 0);
-	}
-
-	return 0;
+static int sm9s5422_led_open(struct inode *inode, struct file *file)
+{
+    int err = 0, i = 0;
+    printk(KERN_CRIT "sm9s5422_led_open, \n");
+    printk("2018440059 Park Jaesun \n");
+    for (i = 0; i < 8; i++)
+    {
+        err = gpio_request(gpy7(i), "Led");
+        if (err)
+            printk("led.c failed to request gpy7(%d) \n", i);
+        s3c_gpio_setpull(gpy7(i), S3C_GPIO_PULL_NONE);
+        gpio_direction_output(gpy7(i), 0);
+    }
+    return 0;
 }
 
-static int sm9s5422_led_release(struct inode * inode, struct file * file){
-	int i=0;
-	printk("sm9s5422_led_release, \n");
+static int sm9s5422_led_release(struct inode *inode, struct file *file)
+{
+    int i = 0;
+    printk("sm9s5422_led_release, \n");
+    for (i = 0; i < 8; i++)
+        gpio_free(gpy7(i));
 
-	for(i=0; i<8; i++)
-		gpio_free(gpy7(i));
-
-	return 0;
+    return 0;
 }
 
-
-
-static ssize_t sm9s5422_led_write(struct file * file, const char * buf, size_t length, loff_t * ofs){
-	int ret=0, i=0;
-	printk("sm9s5422_led_write, \n");
-	unsigned char cbuf[8];
-	ret = copy_from_user(cbuf, buf, length);
-	for(i=0; i<8; i++)
-		gpio_direction_output(gpy7(i), (unsigned int) cbuf[i]);
-	return 0;
+static ssize_t sm9s5422_led_write(struct file *file, const char *buf, size_t length, loff_t *ofs)
+{
+    int ret = 0, i = 0;
+    printk("sm9s5422_led_write, \n");
+    unsigned char cbuf[8];
+    ret = copy_from_user(cbuf, buf, length);
+    for (i = 0; i < 8; i++)
+        gpio_direction_output(gpy7(i), (unsigned int)cbuf[i]);
+    return 0;
 }
-
-
-
-
-
-
-
-
-
 
 static struct file_operations sm9s5422_led_fops = {
-	.owner = THIS_MODULE,
-	.open = sm9s5422_led_open,
-	.release = sm9s5422_led_release,
-	.write = sm9s5422_led_write,
+    .owner = THIS_MODULE,
+    .open = sm9s5422_led_open,
+    .release = sm9s5422_led_release,
+    .write = sm9s5422_led_write,
 };
 
 static struct miscdevice sm9s5422_led_driver = {
-	.minor = MISC_DYNAMIC_MINOR,
-	.name = "sm9s5422_led",
-	.fops = &sm9s5422_led_fops,
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "sm9s5422_led",
+    .fops = &sm9s5422_led_fops,
 };
 
-static int sm9s5422_led_init(void){
-	printk("sm9s5422_led_init, \n");
-	return misc_register(&sm9s5422_led_driver);
+static int sm9s5422_led_init(void)
+{
+    printk("sm9s5422_led_init, \n");
+    return misc_register(&sm9s5422_led_driver);
 }
 
-static void sm9s5422_led_exit(void){
-	printk("sm9s5422_led_exit, \n");
-	misc_deregister(&sm9s5422_led_driver);
+static void sm9s5422_led_exit(void)
+{
+    printk("sm9s5422_led_exit, \n");
+    misc_deregister(&sm9s5422_led_driver);
 }
 
 module_init(sm9s5422_led_init);
